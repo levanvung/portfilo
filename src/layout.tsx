cb76f13ea31e959b7f120c1e5437d4706fd96e7e
vunglv-cntt <levanvung115@gmail.com>
2024-11-17 22:11:42 +0700

    . add

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,17 +1,31 @@
-import { Outlet } from "react-router-dom"
-import AppHeader from "./components/layout/app.header"
-import AppFooter from "./components/layout/app.footer"
-import React from "react"
+import { Outlet, useLocation } from "react-router-dom";
+import AppHeader from "./components/layout/app.header";
+import AppFooter from "./components/layout/app.footer";
+import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import './styles/transition.css';
 
 function Layout() {
+  const location = useLocation();
 
   return (
     <div>
       <AppHeader />
-      <Outlet />
+      <TransitionGroup component={null}>
+        <CSSTransition
+          key={location.pathname}
+          classNames="page"
+          timeout={300}
+        >
+
+          <div className="page-wrapper">
+            <Outlet />
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
       <AppFooter />
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
