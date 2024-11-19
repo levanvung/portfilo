67805cfa51978020f1065466f27f2cc66fa5e055
vung-cntt <levanvung9111@gmail.com>
2024-11-19 09:15:28 +0700

    - remove

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -47,56 +47,56 @@ export const EXPERIENCES = [
     }
 ]
 
-export const PROJECTS = [
-    {
-        id: 1,
-        imgPath: belle,
-        title: "Nhận diện các bệnh ngoài da bằng AI",
-        description: "Belle AI là một dự án giúp người dùng có thể nhận biết và tìm hiểu về các bệnh lí ngoài da bằng công nghệ AI",
-        githubLink: "https://hoidanit.vn/khoa-hoc/react-native-ultimate-phat-trien-ung-dung-mobile-66e7ab4578aaf477418cb538.html",
-        demoLink: "https://demo.belle.ai/"
-    },
-    {
-        id: 2,
-        imgPath: cms,
-        title: "Hệ thống CMS Belle",
-        description: "Là một hệ thống CMS quản lý nội dung cho website chính thức của Belle AI , hỗ trợ người dùng có nhu cầu tìm hiểu hoặc sử dụng sản phẩm ",
-        githubLink: "https://hoidanit.vn/khoa-hoc/java-spring-rest-ultimate-for-beginners-6631cb577353ab6861b188f6.html",
-        demoLink: "https://www.belle.ai/"
-    },
-    {
-        id: 3,
-        imgPath: tool,
-        title: "Tool gán nhãn cho bệnh ngoài da",
-        description: "Một công cụ giúp các Annotators có thể tạo ra các bộ dữ liệu tốt nhất ",
-        githubLink: "https://hoidanit.vn/khoa-hoc/java-spring-mvc-ultimate-for-beginners-65ce0b770c05f4450fbd86ac.html",
-        demoLink: ""
-    },
-    {
-        id: 4,
-        imgPath: app,
-        title: "Hệ thống CMS cho ứng dụng WGHN",
-        description: "Là một mạng xã hội dành cho các golfer, những chức năng bao gồm ( Booking Sân, xem giải đấu, mua bảo hiểm, tính điểm, nhắn tin gọi điện và trải nghiệm một mạng xã hội dành cho các golfer)",
-        githubLink: "https://hoidanit.vn/khoa-hoc/react-pro-max-voi-nextjs-lam-chu-toan-dien-reactjs-hien-dai-65198100e6bafa8caad417a6.html",
-        demoLink: "https://apps.apple.com/vn/app/wghn/id1590376779?l=vi"
-    },
-    {
-        id: 5,
-        imgPath: hio,
-        title: "Website mua bảo hiểm cho golfer",
-        description: "Là một website giúp các golfer có thể dễ dàng mua các gói bảo hiểm HIO theo nhu cầu. Hợp tác với đơn vị cung cấp bảo hiểm uy tín hàng đầu Việt Nam là PVI, giao diện thân thiện đơn giản dễ sử dụng ",
-        githubLink: "https://hoidanit.vn/khoa-hoc/nestjs-voi-typescript-mongodb-sieu-de-64686ec6fb456bbb90663dd6.html",
-        demoLink: "https://hio.wghn.net/"
-    },
-    {
-        id: 6,
-        imgPath: chat,
-        title: "Web chat WGHN",
-        description: "Là một website giúp người dùng có thể liên lạc với nhau qua nền tảng wesite thay vì phải tải app WGHN, đơn giản dễ sử dụng và tiện ích",
-        githubLink: "https://hoidanit.vn/khoa-hoc/react-ultimate-react-co-ban-tu-z-toi-a-640b50b96cc592d780aab976.html",
-        demoLink: "https://chat.wghn.net/"
-    }
-]
+// export const PROJECTS = [
+//     {
+//         id: 1,
+//         imgPath: belle,
+//         title: "Nhận diện các bệnh ngoài da bằng AI",
+//         description: "Belle AI là một dự án giúp người dùng có thể nhận biết và tìm hiểu về các bệnh lí ngoài da bằng công nghệ AI",
+//         githubLink: "https://hoidanit.vn/khoa-hoc/react-native-ultimate-phat-trien-ung-dung-mobile-66e7ab4578aaf477418cb538.html",
+//         demoLink: "https://demo.belle.ai/"
+//     },
+//     {
+//         id: 2,
+//         imgPath: cms,
+//         title: "Hệ thống CMS Belle",
+//         description: "Là một hệ thống CMS quản lý nội dung cho website chính thức của Belle AI , hỗ trợ người dùng có nhu cầu tìm hiểu hoặc sử dụng sản phẩm ",
+//         githubLink: "https://hoidanit.vn/khoa-hoc/java-spring-rest-ultimate-for-beginners-6631cb577353ab6861b188f6.html",
+//         demoLink: "https://www.belle.ai/"
+//     },
+//     {
+//         id: 3,
+//         imgPath: tool,
+//         title: "Tool gán nhãn cho bệnh ngoài da",
+//         description: "Một công cụ giúp các Annotators có thể tạo ra các bộ dữ liệu tốt nhất ",
+//         githubLink: "https://hoidanit.vn/khoa-hoc/java-spring-mvc-ultimate-for-beginners-65ce0b770c05f4450fbd86ac.html",
+//         demoLink: ""
+//     },
+//     {
+//         id: 4,
+//         imgPath: app,
+//         title: "Hệ thống CMS cho ứng dụng WGHN",
+//         description: "Là một mạng xã hội dành cho các golfer, những chức năng bao gồm ( Booking Sân, xem giải đấu, mua bảo hiểm, tính điểm, nhắn tin gọi điện và trải nghiệm một mạng xã hội dành cho các golfer)",
+//         githubLink: "https://hoidanit.vn/khoa-hoc/react-pro-max-voi-nextjs-lam-chu-toan-dien-reactjs-hien-dai-65198100e6bafa8caad417a6.html",
+//         demoLink: "https://apps.apple.com/vn/app/wghn/id1590376779?l=vi"
+//     },
+//     {
+//         id: 5,
+//         imgPath: hio,
+//         title: "Website mua bảo hiểm cho golfer",
+//         description: "Là một website giúp các golfer có thể dễ dàng mua các gói bảo hiểm HIO theo nhu cầu. Hợp tác với đơn vị cung cấp bảo hiểm uy tín hàng đầu Việt Nam là PVI, giao diện thân thiện đơn giản dễ sử dụng ",
+//         githubLink: "https://hoidanit.vn/khoa-hoc/nestjs-voi-typescript-mongodb-sieu-de-64686ec6fb456bbb90663dd6.html",
+//         demoLink: "https://hio.wghn.net/"
+//     },
+//     {
+//         id: 6,
+//         imgPath: chat,
+//         title: "Web chat WGHN",
+//         description: "Là một website giúp người dùng có thể liên lạc với nhau qua nền tảng wesite thay vì phải tải app WGHN, đơn giản dễ sử dụng và tiện ích",
+//         githubLink: "https://hoidanit.vn/khoa-hoc/react-ultimate-react-co-ban-tu-z-toi-a-640b50b96cc592d780aab976.html",
+//         demoLink: "https://chat.wghn.net/"
+//     }
+// ]
 
 export const SKILLS_DATA = [
     'HTML',
